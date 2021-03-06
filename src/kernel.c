7946dbefa509f20c4599242f0a0e565fa86cabd3
avcado <devavcado@gmail.com>
2021-03-06 14:00:00 -0600

    Commentify kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,13 @@
+// include/libc includes
 #include <stdio.h>
+
+// include/ includes
 #include "kerror.h"
 #include "init.h"
 
 void main(void){
+  // Initialize everything VGA.
   terminal_initialize();
+  // Start the init system
   init();
 }
