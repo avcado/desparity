fccbd2e3c303a561ec4ea10f8360ee3ec83ddcc0
avcado <devavcado@gmail.com>
2021-03-06 19:01:13 -0600

    Setup functions for external assembly

--- include/idt.h
+++ include/idt.h
@@ -28,3 +28,20 @@ void remapPIC() {
 }
 
 // External functions we'll call in assembly
+extern int load_idt();
+extern int irq0();
+extern int irq1();
+extern int irq2();
+extern int irq3();
+extern int irq4();
+extern int irq5();
+extern int irq6();
+extern int irq7();
+extern int irq8();
+extern int irq9();
+extern int irq10();
+extern int irq11();
+extern int irq12();
+extern int irq13();
+extern int irq14();
+extern int irq15();
