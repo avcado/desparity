1b49842528af8d3c30fe9472a8da952b9ef86533
avcado <devavcado@gmail.com>
2021-03-07 09:21:56 -0600

    Fix up compile issue

--- include/init.h
+++ include/init.h
@@ -8,18 +8,27 @@
 #include "libc/stdio.h"
 #include "vga.h"
 
-#ifndef INIT
-#define INIT
+void init() {
+    printf("Color test: ");
+    testColors();
+    printcol(VGA_COLOR_LIGHT_BLUE, "\nStarting init system...\n");
+    initGDT();
+}
 
-void init();
-
-#endif //INIT
-
-#ifndef TESTCOL
-#define TESTCOL
-
-// Tests color to make sure that colors
-// are working.  This is also ugly code
-void testColors();
-
-#endif //TESTCOL
+void testColors(){
+    printcol(VGA_COLOR_BLUE, "blue ");
+    printcol(VGA_COLOR_GREEN, "green ");
+    printcol(VGA_COLOR_CYAN, "cyan ");
+    printcol(VGA_COLOR_RED, "red ");
+    printcol(VGA_COLOR_MAGENTA, "magenta ");
+    printcol(VGA_COLOR_BROWN, "brown ");
+    printcol(VGA_COLOR_LIGHT_GREY, "light grey ");
+    printcol(VGA_COLOR_DARK_GREY, "dark grey ");
+    printcol(VGA_COLOR_LIGHT_BLUE, "light blue   ");
+    printcol(VGA_COLOR_LIGHT_GREEN, "light green ");
+    printcol(VGA_COLOR_LIGHT_CYAN, "light cyan ");
+    printcol(VGA_COLOR_LIGHT_RED, "light red ");
+    printcol(VGA_COLOR_LIGHT_MAGENTA, "light magenta ");
+    printcol(VGA_COLOR_LIGHT_BROWN, "light brown ");
+    printcol(VGA_COLOR_WHITE, "white\n");
+}
